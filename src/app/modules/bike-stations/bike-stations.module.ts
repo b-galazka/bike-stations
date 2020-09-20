b5dace007202f57a601afd2f5e8081c2d0cce0d1
b-galazka <bartlomiejgalazka@zoho.eu>
2020-09-20 17:14:26 +0200

    refactor

--- src/app/modules/bike-stations/bike-stations.module.ts
+++ src/app/modules/bike-stations/bike-stations.module.ts
@@ -7,10 +7,10 @@ import { BikeStationsRoutingModule } from './bike-stations-routing.module';
 import { BikeStationComponent } from './components/bike-station/bike-station.component';
 import { DistanceUnitTranslationKeyPipe } from './components/bike-station/pipes/distance-unit-translation-key.pipe';
 import { DistancePipe } from './components/bike-station/pipes/distance.pipe';
-import { BikeStationsListComponent } from './components/bike-stations-list/bike-stations-list.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { BikeStationDetailsPageComponent } from './pages/bike-station-details-page/bike-station-details-page.component';
 import { BikeStationsListPageComponent } from './pages/bike-stations-list-page/bike-stations-list-page.component';
+import { BikeStationsListComponent } from './pages/bike-stations-list-page/components/bike-stations-list/bike-stations-list.component';
 import { BikeStationsService } from './services/bike-stations/bike-stations.service';
 
 @NgModule({
--- src/app/modules/bike-stations/pages/bike-stations-list-page/components/bike-stations-list/bike-stations-list.component.spec.ts
+++ src/app/modules/bike-stations/pages/bike-stations-list-page/components/bike-stations-list/bike-stations-list.component.spec.ts
@@ -1,7 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MockComponent } from 'ng-mocks';
-import { BikeStationComponent } from '../bike-station/bike-station.component';
 
+import { BikeStationComponent } from '@bike-stations/components/bike-station/bike-station.component';
 import { BikeStationsListComponent } from './bike-stations-list.component';
 
 describe('BikeStationsListComponent', () => {
